ce10fd499c3a031a6968ee00f31ba99b743a5c5c
Lev-Janicki <72770454+Lev-Janicki@users.noreply.github.com>
2021-01-12 10:59:09 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+music.set_volume(20)
 while True:
-    if input.sound_level() > 150:
+    if input.light_level() > 10:
         light.show_animation(light.rainbowAnimation, 500)
+        music.pew_pew.play()
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
+music.setVolume(20)
 while (true) {
-    if (input.soundLevel() > 150) {
+    if (input.lightLevel() > 10) {
         light.showAnimation(light.rainbowAnimation, 500)
+        music.pewPew.play()
     } else {
         light.clear()
     }
